aa63deddd7e52d9ee6396606b2bc7015b62fbe1d
Ken Barker <kbarker@wavelynx.com>
2026-02-18 16:22:38 -0700

    Update homebrew sha256 for v0.3.0 GPU fix

--- homebrew/peakmon.rb
+++ homebrew/peakmon.rb
@@ -3,7 +3,7 @@ class Peakmon < Formula
   homepage "https://github.com/Kenbark42/peakmon"
   version "0.3.0"
   url "https://github.com/Kenbark42/peakmon/releases/download/v#{version}/peakmon-v#{version}-aarch64-apple-darwin.tar.gz"
-  sha256 "e0310d8c59ded1b4cf975f32a99d889db9e03b9afe681f72474c96670731d722"
+  sha256 "9597523f5ca48dbbb0f6b97bc6db6c1f7bb3bf8838d611126a57a5f9ee8b1a1f"
   license "MIT"
 
   def install
